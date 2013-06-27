e9e82b9ca22937418518d73728a0386a93def1ac
Goncalo Pereira <goncalo.pereira@7digital.com>
2013-06-27 17:57:16 +0100

    fix keys

--- get_tweets.rb
+++ get_tweets.rb
@@ -1,18 +1,25 @@
 require 'rubygems'
 require 'oauth'
+require 'net/http'
+require 'uri'
 
-consumer_key = ''
-consumer_secret = ''
-token_key = ''
-token_secret = '' 
+@consumer_key = ''
+@consumer_secret = ''
+@token_key = ''
+@token_secret = '' 
 
 require './keys'
 
-consumer = OAuth::Consumer.new(consumer_key,consumer_secret, {
-    :site=>"https://api.twitter.com"
+puts @token_key
+
+url = 'https://api.twitter.com'
+uri = URI.parse(url)
+
+consumer = OAuth::Consumer.new(@consumer_key,@consumer_secret, {
+    :site=>url
     })
 
-token = OAuth::Token.new(token_key,token_secret)
+token = OAuth::Token.new(@token_key,@token_secret)
 
 path = '/1.1/search/tweets.json'
 
@@ -20,6 +27,8 @@ querystring = '?q=7digital'
 
 request = consumer.create_signed_request(:get,path+querystring, token)
 
-puts request.methods
+http = Net::HTTP.new(uri.host, uri.port) 
 
+response = http.request(request)
 
+puts response.body
