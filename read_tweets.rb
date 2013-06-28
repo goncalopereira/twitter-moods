9a973c89d691fb388936a5b97e84c1a630f36ee7
Goncalo Pereira <goncalo.pereira@7digital.com>
2013-06-28 12:30:26 +0100

    adding split by words

--- read_tweets.rb
+++ read_tweets.rb
@@ -4,10 +4,10 @@ results = File.read('results')
 parsed_response_body = JSON.parse(results)
 filtered_list = parsed_response_body['statuses']
 
-def remove_accounts_named name, original_list
+def remove_accounts_named name, list
 	resulting_list = Array.new
 	
-	original_list.each do |status|
+	list.each do |status|
 		if !status['user']['name'].include? name
 			resulting_list  << status
 		end
@@ -16,15 +16,25 @@ def remove_accounts_named name, original_list
 	resulting_list
 end
 
-
+def add_word_list_entries list
+	list.each do |status|
+		words = status["text"].split( )
+		status["words"] = words
+	end
+	
+	list
+end
 
 filtered_list = remove_accounts_named '7digital', filtered_list
+filtered_list = add_word_list_entries filtered_list
 
 i=0
 filtered_list.each do |status|
-	puts status.class
-       i+=1
-       puts "#{i}, #{status["created_at"]}, #{status["lang"]}, #{status["user"]["name"]}, #{status["retweet_count"]}, #{status["text"]}"
+	i+=1
+	puts "#{i}, #{status["created_at"]}, #{status["lang"]}, #{status["user"]["name"]}, #{status["retweet_count"]}, #{status["text"]}"
+	status["words"].each do |word|
+		puts word
+	end
 end
 
 
