4535abd63d81095b8c6f21defbab6f6b90fc7240
Goncalo Pereira <goncalo.pereira@7digital.com>
2013-06-28 12:18:52 +0100

    doesn't need to move to array, already an array

--- read_tweets.rb
+++ read_tweets.rb
@@ -1,16 +1,8 @@
 require 'json'
 
 results = File.read('results')
-
 parsed_response_body = JSON.parse(results)
-
-filtered_list = Array.new
-
-parsed_response_body['statuses'].each do |status|
-	filtered_list << status
-end
-
-#remove 7digital accounts
+filtered_list = parsed_response_body['statuses']
 
 def remove_accounts_named name, original_list
 	resulting_list = Array.new
@@ -25,10 +17,12 @@ def remove_accounts_named name, original_list
 end
 
 
+
 filtered_list = remove_accounts_named '7digital', filtered_list
 
 i=0
 filtered_list.each do |status|
+	puts status.class
        i+=1
        puts "#{i}, #{status["created_at"]}, #{status["lang"]}, #{status["user"]["name"]}, #{status["retweet_count"]}, #{status["text"]}"
 end
