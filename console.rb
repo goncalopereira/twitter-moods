a9644f8066b30b41d7a23410a3c0ed4a5e948818
Goncalo Pereira <goncalo.pereira@7digital.com>
2013-06-28 16:58:01 +0100

    add console app to pick format and file move to func

---
+++ console.rb
@@ -0,0 +1,14 @@
+require './read_tweets'
+
+filtered_list = []
+
+if ARGV[0] == 'json'
+	filtered_list = read_from_json ARGV[1]
+end
+
+if ARGV[0] == 'csv'
+	filtered_list = read_from_csv ARGV[1]
+end
+
+run_twitter_moods filtered_list
+
--- read_tweets.rb
+++ read_tweets.rb
@@ -35,42 +35,45 @@ def read_from_csv filename
 	tweets_json
 end	
 
-filtered_list = read_from_json 'results'
+def run_twitter_moods filtered_list
+	remove_accounts_named '7digital', filtered_list
 
-remove_accounts_named '7digital', filtered_list
+	add_word_list_entries filtered_list
 
-add_word_list_entries filtered_list
-
-remove_short_words filtered_list
+	remove_short_words filtered_list
 
 #hashtags
-remove_words_starting_with '#', filtered_list 
+	remove_words_starting_with '#', filtered_list 
 #urls
-remove_words_starting_with 'http', filtered_list 
+	remove_words_starting_with 'http', filtered_list 
 #accounts
-remove_words_starting_with '@', filtered_list
+	remove_words_starting_with '@', filtered_list
 
-remove_chars_from_words '\'s', filtered_list
-remove_non_alphanumeric filtered_list
+	remove_chars_from_words '\'s', filtered_list
+	remove_non_alphanumeric filtered_list
 
-moods = Hash.new
+	moods = Hash.new
 
-File.open('moods').each do |line|
-	fields = line.split()
-	moods[fields[0]] = fields[1]
-end
+	File.open('moods').each do |line|
+		fields = line.split()
+		moods[fields[0]] = fields[1]
+	end
 
 #add mood to tweet
-filtered_list.each do |status|
-	status["words"].each do |word|
-		total = (moods[word].to_i||0) * (status["retweet_count"].to_i+1) #include original
-		status["mood"] = (status["mood"]||0) + total
-	end	
+	filtered_list.each do |status|
+		status["words"].each do |word|
+			total = (moods[word].to_i||0) * (status["retweet_count"].to_i+1) #include original
+			status["mood"] = (status["mood"]||0) + total
+		end	
+	end
+
+	filtered_list.sort! { |x,y| y["mood"] <=> x["mood"] } 
+
+	filtered_list.each do |status|
+		puts "#{status["user"]["name"]}, #{status["mood"]}, #{status["text"]}"
+	end
 end
 
-filtered_list.sort! { |x,y| y["mood"] <=> x["mood"] } 
 
-filtered_list.each do |status|
-	puts "#{status["user"]["name"]}, #{status["mood"]}, #{status["text"]}"
-end
+
 
