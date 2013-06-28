ae0913c77556b833b1d59ff09e7c17bb4b30d769
Goncalo Pereira <goncalo.pereira@7digital.com>
2013-06-28 14:18:56 +0100

    remove short words, hashtags, urls

--- read_tweets.rb
+++ read_tweets.rb
@@ -5,15 +5,10 @@ parsed_response_body = JSON.parse(results)
 filtered_list = parsed_response_body['statuses']
 
 def remove_accounts_named name, list
-	resulting_list = Array.new
-	
-	list.each do |status|
-		if !status['user']['name'].include? name
-			resulting_list  << status
-		end
+	list.delete_if do |status|
+		 status['user']['name'].include? name
 	end
-	
-	resulting_list
+	list	
 end
 
 def add_word_list_entries list
@@ -21,12 +16,32 @@ def add_word_list_entries list
 		words = status["text"].split( )
 		status["words"] = words
 	end
-	
-	list
+end
+
+def remove_words_starting_with chars, list
+	list.each do |status|
+		status["words"].delete_if do |word|
+			word.start_with?(chars)
+		end
+	end
+end
+
+def remove_short_words list
+	list.each do |status|
+		status["words"].delete_if do |word|
+			word.length <= 2
+		end
+	end
 end
 
 filtered_list = remove_accounts_named '7digital', filtered_list
 filtered_list = add_word_list_entries filtered_list
+filtered_list = remove_short_words filtered_list
+
+#hashtags
+filtered_list = remove_words_starting_with '#', filtered_list 
+#urls
+filtered_list = remove_words_starting_with 'http', filtered_list 
 
 i=0
 filtered_list.each do |status|
