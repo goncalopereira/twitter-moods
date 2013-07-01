9676cf2fa2b2fb76f517d076d9c505945b933fb9
Goncalo Pereira <goncalo.pereira@7digital.com>
2013-07-01 10:46:34 +0100

    move read and writes to io.rb

---
+++ io.rb
@@ -0,0 +1,40 @@
+def read_from_json filename
+        results = File.read(filename)
+        parsed_response_body = JSON.parse(results)
+        parsed_response_body['statuses']
+end
+
+def read_from_csv filename
+        results = []
+        File.open(filename).each do |line|
+                if line.start_with? '<'
+                        results << line
+                end
+        end
+
+        tweets_json = []
+
+        results.each do |line|
+
+                fields = line.split()
+
+
+                fake_tweet = Hash.new
+                fake_tweet["user"] = Hash.new
+                fake_tweet["user"]["name"] = fields[0]
+
+                fake_tweet["text"] = line
+
+                tweets_json << fake_tweet
+        end
+
+        tweets_json
+end
+
+def write_results filename, tweets
+        File.open(filename, 'w') do |file|
+                tweets.each do |status|
+                    file.puts "#{status["user"]["name"]}, #{status["mood"]}, #{status["text"]}"
+                end
+        end
+end
--- read_tweets.rb
+++ read_tweets.rb
@@ -1,47 +1,7 @@
 require 'json'
 require 'csv'
 require './filters'
-
-def read_from_json filename
-	results = File.read(filename)
-	parsed_response_body = JSON.parse(results)
-	parsed_response_body['statuses']
-end
-
-def read_from_csv filename
-	results = []
-	File.open(filename).each do |line|
-		if line.start_with? '<'
-			results << line
-		end
-	end
-
-	tweets_json = []
-
-	results.each do |line|
-		
-		fields = line.split()
-				
-
-		fake_tweet = Hash.new
-		fake_tweet["user"] = Hash.new
-		fake_tweet["user"]["name"] = fields[0]
-
-		fake_tweet["text"] = line
-		
-		tweets_json << fake_tweet				
-	end
-
-	tweets_json
-end	
-
-def write_results filename, tweets		
-        File.open(filename, 'w') do |file| 
-               	tweets.each do |status|
-                    file.puts "#{status["user"]["name"]}, #{status["mood"]}, #{status["text"]}"
-                end
-        end
-end
+require './io'
 
 def run_twitter_moods filtered_list, filename
 	remove_accounts_named '7digital', filtered_list
