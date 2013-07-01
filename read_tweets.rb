0fa41b37add06af94a413ba710bca20fc00d8565
Goncalo Pereira <goncalo.pereira@7digital.com>
2013-07-01 11:29:42 +0100

    fix issue with position tracking,
    add simple moving average

--- read_tweets.rb
+++ read_tweets.rb
@@ -9,14 +9,12 @@ def run_twitter_moods filtered_list, filename
 	add_word_list_entries filtered_list
 
 	remove_short_words filtered_list
-
 #hashtags
 	remove_words_starting_with '#', filtered_list 
 #urls
 	remove_words_starting_with 'http', filtered_list 
 #accounts
 	remove_words_starting_with '@', filtered_list
-
 	remove_chars_from_words '\'s', filtered_list
 	remove_non_alphanumeric filtered_list
 
@@ -26,20 +24,36 @@ def run_twitter_moods filtered_list, filename
 	#add mood and position to tweet
 	i=0
 	filtered_list.each do |status|
+		status["mood"] = 0
+		status["position"] = i
+
 		status["words"].each do |word|
 			total = (moods[word].to_i||0) * (status["retweet_count"].to_i+1) #include original
 			status["mood"] = (status["mood"]||0) + total
-			status["position"] = i
-			i+=1
 		end	
-
+				
 		mood_data << status["mood"]
+
+		i+=1
 	end
 	
+	filtered_list.each do |status|
+
+		current_position = status["position"]			
+		
+		if current_position >= 4
+			moving_average = (filtered_list[current_position]["mood"]+filtered_list[current_position-1]["mood"]+filtered_list[current_position-2]["mood"]+filtered_list[current_position-3]["mood"]+filtered_list[current_position-4]["mood"])/5
+
+			puts moving_average
+		end
+	
+			
+	end	
+	
+	#write results
 	write_results filename+"_unsorted_results", filtered_list	
 
 	filtered_list.sort! { |x,y| y["mood"] <=> x["mood"] } 
-
 	write_results filename+"_sorted_results", filtered_list
 end
 
